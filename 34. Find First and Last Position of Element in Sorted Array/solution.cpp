552e93b2fb7bd296e7dda4d2ee36bc4ad7bcd9a0
0x00-V <93012418+0x00-V@users.noreply.github.com>
2024-12-12 02:06:08 +0000

    .

---
+++ 34. Find First and Last Position of Element in Sorted Array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start{-1};
+        int end{-1};
+        for(int i{0}; i < nums.size(); i++)
+        {
+            if(nums[i] == target)
+            {
+                if(start == -1)
+                {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+        vector<int> res;
+        res.push_back(start);
+        res.push_back(end);
+        return res;
+    }
+};
